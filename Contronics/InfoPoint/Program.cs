d1e8348051b0b46055c85724555f0b7c5eed3595
Ionut Pac-Balaj <ipacbalaj@gmail.com>
2025-03-01 13:08:04 +0200

    Create InfoPoint console app and connect it to the signalR hub

---
+++ Contronics/InfoPoint/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+string hubUrl = "http://localhost:5244/sensorDataHub"; 
+
+var connection = new HubConnectionBuilder()
+    .WithUrl(hubUrl)
+    .Build();
+
+connection.On<string, double, DateTime>("ReceiveSensorData", (sensorId, value, timestamp) =>
+{
+    Console.WriteLine($"Sensor: {sensorId}, Value: {value}, Timestamp: {timestamp}");
+});
+
+async Task StartConnectionAsync()
+{
+    try
+    {
+        await connection.StartAsync();
+        Console.WriteLine("Connected to SignalR hub.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error connecting to hub: {ex.Message}");
+    }
+}
+
+await StartConnectionAsync();
+
+// Keep the console app running
+await Task.Delay(-1);
---
+++ Contronics/readme.md
@@ -0,0 +1,5 @@
+## SignalR communication fails
+1. What happens when InfoPoint is down?
+    - will you know that the message was not received? 
+    - did you implement any acknowledge method? 
+2. What happens if BrokerService is down?
