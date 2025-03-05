a34b5c06d14285cf515fa756390b63be104a3e04
Ionut Pac-Balaj <ipacbalaj@gmail.com>
2025-03-05 08:57:41 +0000

    Add logging in the queue consumer

--- Contronics/InfoPointWeb/SensorDataConsumer.cs
+++ Contronics/InfoPointWeb/SensorDataConsumer.cs
@@ -1,13 +1,17 @@
 using MassTransit;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using Shared;
 
 namespace InfoPointWeb;
 
-public class SensorDataConsumer: IConsumer<SensorDataMessage>
+public class SensorDataConsumer : IConsumer<SensorDataMessage>
 {
     public async Task Consume(ConsumeContext<SensorDataMessage> context)
-    {
+    {   
+        Log.Information("Received sensor data message via queue: {Message}", context.Message);
         var message = context.Message;
-        await Task.CompletedTask; // Simulate async work if needed   
+        // Simulate async work if needed
+        await Task.CompletedTask;  
     }
 }
